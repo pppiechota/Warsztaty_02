990578018893e19fb4dd5dd4c4dc4549614f4247
Piotr Piechota <piotr_piechota@poczta.onet.pl>
2019-12-06 14:36:42 +0100

    UserDao create method

--- src/com/piotrpiechota/UserDao.java
+++ src/com/piotrpiechota/UserDao.java
@@ -1,6 +1,39 @@
 package com.piotrpiechota;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class UserDao {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String READ_USER_QUERY =
+            "SELECT id, username, email, password FROM users where id = ?";
+    private static final String UPDATE_USER_QUERY =
+            "UPDATE users SET username = ?, email = ?, password = ? where id = ?";
+    private static final String DELETE_USER_QUERY =
+            "DELETE FROM users WHERE id = ?";
+    private static final String FIND_ALL_USERS_QUERY =
+            "SELECT id, username, email, password FROM users";
 
-
+    public User create(User user) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getPassword());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
