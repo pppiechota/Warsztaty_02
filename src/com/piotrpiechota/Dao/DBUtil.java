af3b5ea1536d95f8860607f2f3eb8d1424ed5273
Piotr Piechota <57040700+pppiechota@users.noreply.github.com>
2020-02-18 17:11:02 +0100

    Hide credentials.

--- src/com/piotrpiechota/Dao/DBUtil.java
+++ src/com/piotrpiechota/Dao/DBUtil.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBUtil {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8";
-    private static final String DB_USER = "root";
-    private static final String DB_PASS = "coderslab";
+    private static final String DB_USER = "***";
+    private static final String DB_PASS = "***";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
